1d57a7862eddef7bdc4cc133cbaaa0aed6068b95
yuriy <mrlazgen@gmail.com>
2017-09-24 17:23:33 +0700

    + Third work - attributes

---
+++ PToE/Work 3/AttributeTry/Loader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace AttributeTry
+{
+    [Source (URL = "http://csharpcorner.mindcrackerinc.netdna-cdn.com/article/round-image-in-xamarin-forms/Images/image005.jpg") ]
+    class Loader
+    {
+        public int load()
+        {
+
+            var type = this.GetType();
+            if(Attribute.IsDefined(type, typeof(Source)))
+            {
+                var source = Attribute.GetCustomAttribute(type, typeof(Source)) as Source;
+                var name = source.URL.Substring(source.URL.LastIndexOf("/") + 1, source.URL.Length - source.URL.LastIndexOf("/")-1);
+
+                Console.WriteLine("URL: {0}", source.URL);
+                Console.WriteLine("Name: {0}", name);
+
+                WebClient webClient = new WebClient();
+                webClient.DownloadFile(source.URL, @"C:\Users\yuriy2\Documents\" + name);
+            } 
+
+            return 0;
+        }
+    }
+}
---
+++ PToE/Work 3/AttributeTry/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributeTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loader loader = new Loader();
+            loader.load();
+        }
+    }
+}
