3b8527675d58b86832db7ab1c1fe47007d2ef9b9
yuriy <mrlazgen@gmail.com>
2017-10-11 11:47:33 +0700

    * renamed Form1 to Facade

---
+++ PToE/Work 5/WeatherLoader/Facade.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WeatherLoader
+{
+    public partial class Facade : Form
+    {
+        Loader loader = new Loader();
+        Parser parser = new Parser();
+        WeatherInfo weather = new WeatherInfo();
+
+        public Facade()
+        {
+            InitializeComponent();
+        }
+
+        private void updateWeather()
+        {
+            weather = parser.parse(loader.load());
+
+            label4.Text = weather.description;
+            label5.Text = weather.current_date;
+            label8.Text = weather.current_time;
+            label6.Text = weather.current_temp;
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            updateWeather();
+        }
+    }
+}
