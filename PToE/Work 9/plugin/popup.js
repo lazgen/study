6a18f24e98dc276f8829787d54ac64d2b821c1ff
Yuriy <mrlazgen@gmail.com>
2017-12-27 17:16:03 +0700

    * implemented plugin

--- PToE/Work 9/plugin/popup.js
+++ PToE/Work 9/plugin/popup.js
@@ -47,90 +47,48 @@ function getCurrentTabUrl(callback) {
   // alert(url); // Shows "undefined", because chrome.tabs.query is async.
 }
 
-/**
- * Change the background color of the current page.
- *
- * @param {string} color The new background color.
- */
-function changeImagesOpasity(opasity) {
-  var images = document.images;
-  var script;
-  for (var i = images.length - 1; i >= 0; i--) {
-    var scriptI = 'document.images[' + i + '].style.opasity=' + opasity+ ';';
-    alert(scriptI);
-    script += scriptI;
-  }
-  //var script = 'document.body.style.backgroundColor="' + color + '";';
+function changeImagesOpasity(opacity) {
 
-  // See https://developer.chrome.com/extensions/tabs#method-executeScript.
-  // chrome.tabs.executeScript allows us to programmatically inject JavaScript
-  // into a page. Since we omit the optional first argument "tabId", the script
-  // is inserted into the active tab of the current window, which serves as the
-  // default.
+  var script = 'for(i=0;i<document.images.length;i++)document.images[i].style.opacity='+opacity+';';
+
   chrome.tabs.executeScript({
     code: script
   });
 }
 
-/**
- * Gets the saved background color for url.
- *
- * @param {string} url URL whose background color is to be retrieved.
- * @param {function(string)} callback called with the saved background color for
- *     the given url on success, or a falsy value if no color is retrieved.
- */
-function getSavedBackgroundColor(url, callback) {
-  // See https://developer.chrome.com/apps/storage#type-StorageArea. We check
-  // for chrome.runtime.lastError to ensure correctness even when the API call
-  // fails.
-  chrome.storage.sync.get(url, (items) => {
-    callback(chrome.runtime.lastError ? null : items[url]);
+function hightlightUrls(value) {
+  var color = 'transparent'; 
+  
+  if(value)
+    color = 'yellow'; 
+
+  var script = 'for(i=0;i<document.links.length;i++)document.links[i].style.backgroundColor="' +color+ ' ";';
+
+  chrome.tabs.executeScript({
+    code: script
   });
 }
 
-/**
- * Sets the given background color for url.
- *
- * @param {string} url URL for which background color is to be saved.
- * @param {string} color The background color to be saved.
- */
-function saveBackgroundColor(url, color) {
-  var items = {};
-  items[url] = color;
-  // See https://developer.chrome.com/apps/storage#type-StorageArea. We omit the
-  // optional callback since we don't need to perform any action once the
-  // background color is saved.
-  chrome.storage.sync.set(items);
-}
-
-// This extension loads the saved background color for the current tab if one
-// exists. The user can select a new background color from the dropdown for the
-// current page, and it will be saved as part of the extension's isolated
-// storage. The chrome.storage API is used for this purpose. This is different
-// from the window.localStorage API, which is synchronous and stores data bound
-// to a document's origin. Also, using chrome.storage.sync instead of
-// chrome.storage.local allows the extension data to be synced across multiple
-// user devices.
 document.addEventListener('DOMContentLoaded', () => {
   getCurrentTabUrl((url) => {
     var dropdown = document.getElementById('dropdown');
+    var checkbox = document.getElementById('highlightCheckBox');
 
-    // Load the saved background color for this page and modify the dropdown
-    // value, if needed.
-    // getSavedBackgroundColor(url, (opa) => {
-    //   if (savedColor) {
-    //     changeBackgroundColor(savedColor);
-    //     dropdown.value = savedColor;
-    //   }
-    // });
+    dropdown.value = 0.5;
+    checkbox.checked = true;
 
-    dropdown.value = 50;
+    changeImagesOpasity(dropdown.value);
+    hightlightUrls(true);
 
     // Ensure the background color is changed and saved when the dropdown
     // selection changes.
     dropdown.addEventListener('change', () => {
       changeImagesOpasity(dropdown.value);
-      //saveBackgroundColor(url, dropdown.value);
     });
+
+    checkbox.addEventListener('change', () => {
+      hightlightUrls(checkbox.checked);
+    });
+
   });
 });
