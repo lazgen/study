9088a2dc4c97e2df7fe8968143b22a052e5f64b7
yuriy <mrlazgen@gmail.com>
2018-02-18 19:39:31 +0700

    * some fixes

--- CS/lab1/task4.m
+++ CS/lab1/task4.m
@@ -1,5 +1,8 @@
-rows = round(rand(1)*4 + 1);
-cols = round(rand(1)*4 + 1);
+rows = randi([1 5]);
+cols = randi([1 5]);
 mat = zeros(rows,cols);
-
 disp(mat);
+
+rows = randi([1 5]);
+cols = randi([1 5]);
+zeros(rows,cols);
